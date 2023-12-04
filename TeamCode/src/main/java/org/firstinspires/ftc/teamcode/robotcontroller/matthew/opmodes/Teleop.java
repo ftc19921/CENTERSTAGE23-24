caf9f83fc52fcfef60fa06f5e454af297000a78c
MatthewWHuang <thematthewhuang@gmail.com>
2023-12-04 18:44:41 -0500

    Camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcontroller/matthew/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcontroller/matthew/opmodes/Teleop.java
@@ -4,19 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robotcontroller.matthew.Robot;
+import org.firstinspires.ftc.teamcode.robotcontroller.matthew.mechanisms.Camera;
 
 @TeleOp
 public class Teleop extends OpMode {
     Robot robot = new Robot();
+    Camera camera = new Camera();
     @Override
     public void init() {
         robot.init(hardwareMap);
+        camera.init(hardwareMap);
     }
 
     @Override
     public void loop() {
 //        robot.tankDrive.drive(gamepad1.left_stick_y, gamepad1.left_trigger-gamepad1.right_trigger);
         robot.macunumDrive.drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.left_trigger-gamepad1.right_trigger);
-//        robot.drone.launch(gamepad1.y);
+        telemetry.addLine(camera.getDebugString());
+        //        robot.drone.launch(gamepad1.y);
     }
 }
