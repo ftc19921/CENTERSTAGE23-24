6cb18d6ad6d3a7b4b4059c3835b7f37e4ec448e0
MatthewWHuang <thematthewhuang@gmail.com>
2023-12-04 19:23:59 -0500

    Fixed casing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcontroller/mechanisms/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcontroller/mechanisms/Camera.java
@@ -21,9 +21,9 @@ public class Camera implements Mechanism {
     OpenCvWebcam webcam;
 
     double Phase;
-    double Rightcol;
-    double leftcol;
-    double midcol;
+    double rightCol;
+//    double leftCol;
+    double midCol;
 
     String debugString;
     int location;
@@ -69,31 +69,31 @@ public class Camera implements Mechanism {
         Mat RightBlueValue;
         Mat LeftRedValue;
         Mat LeftBlueValue;
-        Mat MiddlefinalBlue = new Mat();
-        Mat MiddlefinalRed = new Mat();
-        Mat LeftfinalBlue = new Mat();
-        Mat LeftfinalRed = new Mat();
-        Mat RightfinalBlue = new Mat();
-        Mat RightfinalRed = new Mat();
+        Mat MiddleFinalBlue = new Mat();
+        Mat MiddleFinalRed = new Mat();
+//        Mat LeftfinalBlue = new Mat();
+//        Mat LeftfinalRed = new Mat();
+        Mat RightFinalBlue = new Mat();
+        Mat RightFinalRed = new Mat();
 
-        double MiddleBlueAvgfin;
-        double MiddleRedAvgfin;
-        double RightBlueAvgfin;
-        double RightRedAvgfin;
-        double LeftBlueAvgfin;
-        double LeftRedAvgfin;
+        double MiddleBlueAvgFin;
+        double MiddleRedAvgFin;
+        double RightBlueAvgFin;
+        double RightRedAvgFin;
+//        double LeftBlueAvgfin;
+//        double LeftRedAvgfin;
 
         Mat outPut = new Mat();
 
 
-        Scalar Middleblue = new Scalar(0.0, 0.0, 255.0);
-        Scalar Middlered = new Scalar(255.0, 0.0, 0.0);
+        Scalar MiddleBlue = new Scalar(0.0, 0.0, 255.0);
+        Scalar MiddleRed = new Scalar(255.0, 0.0, 0.0);
 
-        Scalar Rightblue = new Scalar(0.0, 0.0, 255.0);
-        Scalar Rightred = new Scalar(255.0, 0.0, 0.0);
+        Scalar RightBlue = new Scalar(0.0, 0.0, 255.0);
+        Scalar RightRed = new Scalar(255.0, 0.0, 0.0);
 
-        Scalar Leftblue = new Scalar(0.0, 0.0, 255.0);
-        Scalar Leftred = new Scalar(255.0, 0.0, 0.0);
+//        Scalar Leftblue = new Scalar(0.0, 0.0, 255.0);
+//        Scalar Leftred = new Scalar(255.0, 0.0, 0.0);
 
         @Override
         public Mat processFrame(Mat input) {
@@ -114,11 +114,11 @@ public class Camera implements Mechanism {
 
 
             input.copyTo(outPut);
-            Imgproc.rectangle(outPut, MiddleRectBlue, Middleblue, 2);
-            Imgproc.rectangle(outPut, MiddleRectRed, Middlered, 2);
+            Imgproc.rectangle(outPut, MiddleRectBlue, MiddleBlue, 2);
+            Imgproc.rectangle(outPut, MiddleRectRed, MiddleRed, 2);
 
-            Imgproc.rectangle(outPut, RightRectBlue, Rightblue, 2);
-            Imgproc.rectangle(outPut, RightRectRed, Rightred, 2);
+            Imgproc.rectangle(outPut, RightRectBlue, RightBlue, 2);
+            Imgproc.rectangle(outPut, RightRectRed, RightRed, 2);
 
 //            Imgproc.rectangle(outPut, LeftRectBlue, Leftblue, 2);
 //            Imgproc.rectangle(outPut, LeftRectRed, Leftred, 2);
@@ -133,40 +133,40 @@ public class Camera implements Mechanism {
 //            LeftBlueValue = YCBCr.submat(LeftRectBlue);
 //            LeftRedValue = YCBCr.submat(LeftRectRed);
 
-            Core.extractChannel(MiddleBlueValue, MiddlefinalBlue, 2);
-            Core.extractChannel(MiddleRedValue, MiddlefinalRed, 0);
-            Core.extractChannel(RightBlueValue, RightfinalBlue, 2);
-            Core.extractChannel(RightRedValue, RightfinalRed, 0);
+            Core.extractChannel(MiddleBlueValue, MiddleFinalBlue, 2);
+            Core.extractChannel(MiddleRedValue, MiddleFinalRed, 0);
+            Core.extractChannel(RightBlueValue, RightFinalBlue, 2);
+            Core.extractChannel(RightRedValue, RightFinalRed, 0);
 //            Core.extractChannel(LeftBlueValue, LeftfinalBlue, 2);
 //            Core.extractChannel(LeftRedValue, LeftfinalRed, 0);
 
-            Scalar MiddleBlueAvg = Core.mean(MiddlefinalBlue);
-            Scalar MiddleRedAvg = Core.mean(MiddlefinalRed);
-            Scalar RightBlueAvg = Core.mean(RightfinalBlue);
-            Scalar RightRedAvg = Core.mean(RightfinalRed);
+            Scalar MiddleBlueAvg = Core.mean(MiddleFinalBlue);
+            Scalar MiddleRedAvg = Core.mean(MiddleFinalRed);
+            Scalar RightBlueAvg = Core.mean(RightFinalBlue);
+            Scalar RightRedAvg = Core.mean(RightFinalRed);
 //            Scalar LeftBlueAvg = Core.mean(LeftfinalBlue);
 //            Scalar LeftRedAvg = Core.mean(LeftfinalRed);
 
-            RightBlueAvgfin = RightBlueAvg.val[0];
-            RightRedAvgfin = RightRedAvg.val[0];
+            RightBlueAvgFin = RightBlueAvg.val[0];
+            RightRedAvgFin = RightRedAvg.val[0];
 //            LeftBlueAvgfin = LeftBlueAvg.val[0];
 //            LeftRedAvgfin = LeftRedAvg.val[0];
-            MiddleBlueAvgfin = MiddleBlueAvg.val[0];
-            MiddleRedAvgfin = MiddleRedAvg.val[0];
+            MiddleBlueAvgFin = MiddleBlueAvg.val[0];
+            MiddleRedAvgFin = MiddleRedAvg.val[0];
 //            telemetry.addData("RB", RightBlueAvgfin);
 //            telemetry.addData("RR", RightRedAvgfin);
 //            telemetry.addData("MB", BlueAvgfin);
 //            telemetry.addData("MR", RedAvgfin);
 //            telemetry.addData("LB", LeftBlueAvgfin);
 //            telemetry.addData("LR", LeftRedAvgfin);
-            Rightcol = Math.max(RightBlueAvgfin, RightRedAvgfin);
-            midcol = Math.max(MiddleBlueAvgfin, MiddleRedAvgfin);
-            leftcol = Math.max(LeftBlueAvgfin, LeftRedAvgfin);
-            if (Rightcol >= tolerance && Rightcol >= midcol) {
+            rightCol = Math.max(RightBlueAvgFin, RightRedAvgFin);
+            midCol = Math.max(MiddleBlueAvgFin, MiddleRedAvgFin);
+//            leftCol = Math.max(LeftBlueAvgfin, LeftRedAvgfin);
+            if (rightCol >= tolerance && rightCol >= midCol) {
                 debugString = "Team prop is on the Right";
                 location = 1;
 //                telemetry.addLine("Team prop is on the Left");
-            } else if (midcol > tolerance && midcol >= Rightcol) {
+            } else if (midCol > tolerance && midCol >= rightCol) {
                 debugString = "Team prop is on the Middle";
                 location = 0;
 //                telemetry.addLine("Team prop is on the Right");
@@ -176,10 +176,10 @@ public class Camera implements Mechanism {
 //                telemetry.addLine("Team prop is on the Middle");
             }
             
-            debugString += "RB" + (int) RightBlueAvgfin;
-            debugString += "RR" + (int) RightRedAvgfin;
-            debugString += "MB" + (int) MiddleBlueAvgfin;
-            debugString += "MR" + (int) MiddleRedAvgfin;
+            debugString += "RB" + (int) RightBlueAvgFin;
+            debugString += "RR" + (int) RightRedAvgFin;
+            debugString += "MB" + (int) MiddleBlueAvgFin;
+            debugString += "MR" + (int) MiddleRedAvgFin;
 //            location += "LB" + (int) LeftBlueAvgfin;
 //            location += "LR" + (int) LeftRedAvgfin;
 
