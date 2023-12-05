d2cbf5b812438f4da1711cb019f3f26ea4619248
morpheus221 <90988397+morpheus221@users.noreply.github.com>
2023-12-04 19:00:57 -0500

    added Mathews mechanism class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Mechanism.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.robotcontroller.matthew.mechanisms;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public interface Mechanism {
+    void init(HardwareMap hardwareMap);
+}
