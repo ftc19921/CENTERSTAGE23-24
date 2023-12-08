7934de660ebdd0a1750d194d6e27b555259d0580
morpheus221 <90988397+morpheus221@users.noreply.github.com>
2023-12-08 10:06:33 -0500

    auto camera is working and blank

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoMark1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoMark1.java
@@ -21,7 +21,7 @@ import org.openftc.easyopencv.OpenCvWebcam;
 public class AutoMark1 extends OpMode {
     Robot robot = new Robot();
     int Stage;
-
+    int framesRan;
     int Location;
     double distance;
     double Forward;
@@ -37,165 +37,78 @@ public class AutoMark1 extends OpMode {
     public double RightBlueAvgfin;
     public double BlueAvgfin;
     public double RedAvgfin;
+
     @Override
     public void init() {
         robot.init(hardwareMap);
-
         CameraOn = 20;
         Pause = 0;
-        Location = 0;
+        framesRan = 0;
         int Stage = 0;
 
     }
 
 
-
-
     public void loop() {
-
-
-
-    if(Pause<1) {
-        if (Location == 10) {
-            switch (Stage) {
-                case 0:
-                    distance = 10185;
-                    Forward = 0.5;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 1:
-                    distance = 10185;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-            }
-        } else if (Location == 20) {
-            switch (Stage) {
-                case 0:
-                    distance = 8912;
-                    Forward = 0.1;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 1:
-                    distance = 8912;
-                    Forward = -0.1;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 2:
-                    distance = 10000;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = 0;
-
-                    break;
-            }
-        } else if(Location==30){
-            switch (Stage) {
-                case 0:
-                    distance = 7812;
-                    Forward = 0.2;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 1:
-                    distance = 6000;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = 0.3;
-
-                    break;
-
-                case 2:
-                    distance = 1900;
-                    Forward = 0.2;
-                    Sideways = 0;
-                    Rotation = 0;
-
-                    break;
-                case 3:
-                    distance = 1900;
-                    Forward = -0.2;
-                    Sideways = 0;
-                    Rotation = 0;
-
-                    break;
-                case 4:
-                    distance = 6000;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = -0.3;
-
-                    break;
-
-                case 5:
-                    distance = 10822;
-                    Forward = 0.2;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 6:
-                    distance = 10000;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = 0.3;
-
-                    break;
-                case 7:
-                    distance = 19735;
-                    Forward = 0.5;
-                    Sideways = 0;
-                    Rotation = 0;
-                    break;
-                case 8:
-                    distance = 10000;
-                    Forward = 0;
-                    Sideways = 0;
-                    Rotation = 0;
-
-                    break;
-
-            }
+        telemetry.addData("Location", robot.camera.getDebugString());
+        framesRan++;
+        if(framesRan<4) {
+            Location = robot.camera.getLocation();
         }
-    }else{
-        Pause--;
-        distance = 10000;
-        Forward = 0;
-        Sideways = 0;
-        Rotation = 0;
-    }
+        telemetry.addData("location",Location);
+        if (Pause < 1) {
+            if (Location == -1) {
+                switch (Stage) {
+                    case 0:
+
+                        break;
+                }
+            } else if (Location == 0) {
+                switch (Stage) {
+                    case 0:
+
+
+                        break;
+                }
+            } else if (Location == 1) {
+                switch (Stage) {
+                    case 0:
+
+
+                        break;
+
+                }
+            }
+        } else {
+            Pause--;
+            distance = 10000;
+            Forward = 0;
+            Sideways = 0;
+            Rotation = 0;
+        }
         double odometryX = robot.mecanumDrive.odometryX;
         double odometryY = robot.mecanumDrive.odometryY;
 
-        auto(distance,Forward,Sideways,Rotation);
-        telemetry.addData("Distance",Math.abs(odometryX)+Math.abs(odometryY));
+        auto(distance, Forward, Sideways, Rotation);
+        telemetry.addData("Distance", Math.abs(odometryX) + Math.abs(odometryY));
     }
 
 
-
-
-    public void auto(double Distance, double forwardPower, double sidewaysPower, double rotationalPower){
+    public void auto(double Distance, double forwardPower, double sidewaysPower, double rotationalPower) {
         robot.mecanumDrive.updateOdometry();
         double odometryX = robot.mecanumDrive.odometryX;
         double odometryY = robot.mecanumDrive.odometryY;
-        if(Math.abs(odometryX)+Math.abs(odometryY)<Distance){
+        if (Math.abs(odometryX) + Math.abs(odometryY) < Distance) {
             robot.mecanumDrive.updateOdometry();
-            robot.mecanumDrive.Drive(forwardPower, sidewaysPower,rotationalPower,false);
-        }else{
+            robot.mecanumDrive.Drive(forwardPower, sidewaysPower, rotationalPower, false);
+        } else {
             Stage++;
-            robot.init(hardwareMap);
-            Pause=20;
+
+            Pause = 20;
         }
 
 
     }
-
-
-
-
 
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.Arm;
+import org.firstinspires.ftc.teamcode.mechanisms.Camera;
 import org.firstinspires.ftc.teamcode.mechanisms.Intake;
 import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
 
@@ -10,12 +11,13 @@ public class Robot {
     public MecanumDrive mecanumDrive = new MecanumDrive();
     public Intake intake = new Intake();
     public Arm arm = new Arm();
+    public Camera camera = new Camera();
 
     public void init(HardwareMap hardwareMap){
        mecanumDrive.init(hardwareMap);
        intake.init(hardwareMap);
        arm.init(hardwareMap);
-
+       camera.init(hardwareMap);
     }
 
 }
